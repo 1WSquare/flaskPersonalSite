9abebd88613873e3233761eb25128a2f2d12d0cd
Will Wasley <willwasley@gmail.com>
2022-10-14 18:24:49 -0400

    fix app.run()

--- main.py
+++ main.py
@@ -8,5 +8,5 @@ def greet():
     return render_template("index.html")
 
 
-if __name__ == '__main__':
-    app.run(host="localhost", port=8080, debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
